3636a17e3592bc33951788761a6d66f445237de9
zoop1259 <kdm432191@gmail.com>
2021-11-05 22:08:55 +0900

    Initial Commit

---
+++ dynamic_table_view_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dynamic_table_view_practice
+//
+//  Created by 강대민 on 2021/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
